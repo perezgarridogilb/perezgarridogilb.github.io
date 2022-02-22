3e055612a60a1883237f03bd11c9fd28cd8746ac
Gilberto <Betho6990@gmail.com>
2022-02-21 18:49:53 -0600

    Bar and margin, Gilberto P. CV.pdf

--- preloader.js
+++
@@ -1,4 +0,0 @@
-window.onload=function(){
-    $('#preloader').delay(100).fadeOut();
-    $('body').removeClass('hidden');
-}    
--- scroll.js
+++
@@ -1,5 +0,0 @@
-window.addEventListener("scroll", function(){
-var header = document.getElementById("header");
-var nav = document.getElementById("ideee").offsetTop;
-    header.classList.toggle("abajo",window.pageYOffset > nav);
-})
